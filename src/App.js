f9045f852f2e44be8b85614e36bf45b23ba384e8
Arman Tehrani <arman.t.tehrani@gmail.com>
2025-02-05 09:43:32 +0330

    route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TicTacToe from "./TicTacToe"; // مطمئن شوید که مسیر درست است
+import TicTacToe from "./TicTacToe";
 import "./App.css";
 
 function App() {
