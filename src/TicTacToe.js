eff3a95d15f64cc066873b6c77897c953c296fbd
Arman Tehrani <arman.t.tehrani@gmail.com>
2025-02-05 10:45:27 +0330

    add player and current player

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,27 +1,47 @@
-import Logo from './asset/Logo.svg';
-import React, { useState } from 'react';
-import { Board , X , O } from './icons/index'
+import React, { useState } from "react";
+import { Board, X, O } from "./components/icons/index";
 
 function TicTacToe() {
-    const [history] = useState([
-        { board: Array(9).fill(null) },
-      ]);
-      const [step] = useState(0);
-      const currentBoard = history[step].board;
+  const [history, setHistory] = useState([{ board: Array(9).fill(null) }]);
+
+  const [step, setStep] = useState(0);
+
+  const handleClick = (index) => {
+    const newBoard = currentBoard.slice();
+    newBoard[index] = currentPlayer;
+
+    const newHistory = history.slice(0, step + 1).concat({
+      board: newBoard,
+      player: currentPlayer === "X" ? "O" : "X",
+    });
+
+    setHistory(newHistory);
+    setStep(newHistory.length - 1);
+  };
+  const currentBoard = history[step].board;
+  const currentPlayer = history[step].player;
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center">Tic Tac Toe <span className='text-sm'>armani</span></h1>
-      <div className="board grid grid-cols-3 w-2/5 mx-auto gap-5 rounded-xl border-b-[1rem] border-r-8 border-l-[1rem] border-t-8 border-black">
+      <h1 className="text-3xl font-bold underline text-center">
+        Tic Tac Toe <span className="text-sm">armani</span>
+      </h1>
+      <div className="board grid grid-cols-3 w-[90%] sm:w-1/3 mx-auto gap-2 rounded-xl border-b-[1rem] border-r-8 border-l-[1rem] border-t-8 border-black">
         {currentBoard.map((cell, index) => (
-          <Board className="w-24 h-24 mx-auto" key={index}>{cell}</Board>
+          <button
+            className="border border-red-900 rounded-md p-3 h-24"
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {cell}
+          </button>
         ))}
       </div>
-      <div className='flex flex-col'>
-      <p>winner : </p>
-      <button className='text-left w-24'>Undo</button>
-      <button className='text-left w-24'>Redo</button>
-      <button className='text-left w-24'>Reset</button>
-      <h3>Move History</h3>
+      <div className="flex flex-col">
+        <p>winner : </p>
+        <button className="text-left w-24">Undo</button>
+        <button className="text-left w-24">Redo</button>
+        <button className="text-left w-24">Reset</button>
+        <h3>Move History</h3>
       </div>
     </div>
   );
